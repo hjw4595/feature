5cef986458ef2965f7e33f1420d2a15ebd83bd8b
jungwanhong <jungwan_hong@lodics.com>
2024-06-25 13:51:30 +0900

    feature - finder 추가

--- components/common/Finder/ResultList.tsx
+++ components/common/Finder/ResultList.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 import { SearchListItem } from ".";
 
+<<<<<<< Updated upstream
 const ResultList = ({ list }: { list: SearchListItem[] }) => {
   let viewList: SearchListItem[] = [];
 
@@ -24,10 +25,54 @@ const ResultList = ({ list }: { list: SearchListItem[] }) => {
       setCount((prev) => prev + 1);
     }
   }, []);
+=======
+interface Props {
+  list: SearchListItem[];
+  page: number;
+  onChangeNextList: () => void;
+  onClick: (value: string) => void;
+}
+
+const ResultList = ({ list, page, onChangeNextList, onClick }: Props) => {
+  const [renderItems, setRenderItems] = useState<SearchListItem[]>([]);
+  const [isNextList, setIsNextList] = useState<boolean>(true);
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+
+  const loader = useRef(null);
+
+  let isList: Boolean = renderItems.length < 0;
+
+  const handleObserver = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      const target = entries[0];
+      if (target.isIntersecting) {
+        onChangeNextList();
+      }
+    },
+    [onChangeNextList]
+  );
+
+  const handleClose = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.stopPropagation();
+    setIsOpen(false);
+  };
+  const handleOpen = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.stopPropagation();
+    setIsOpen(true);
+  };
+
+  const handleClick = useCallback(
+    (value: string) => {
+      onClick(value);
+    },
+    [onClick]
+  );
+>>>>>>> Stashed changes
 
   useEffect(() => {
     const option = {
       root: null,
+<<<<<<< Updated upstream
       rootMargin: "20px",
       threshold: 0,
     };
@@ -48,7 +93,75 @@ const ResultList = ({ list }: { list: SearchListItem[] }) => {
         </>
       ))}
     </div>
+=======
+      rootMargin: "0px",
+      threshold: 0,
+    };
+    const observer = new IntersectionObserver(handleObserver, option);
+    if (loader.current) {
+      observer.observe(loader.current);
+    }
+
+    return () => {
+      if (observer) observer.disconnect();
+    };
+  }, [handleObserver]);
+
+  useEffect(() => {
+    const nowRenderList = list.slice(0, page * 5);
+    setRenderItems(nowRenderList);
+    setIsNextList(nowRenderList.length < list.length);
+    setIsOpen(true);
+  }, [list, page]);
+
+  return (
+    <>
+      <div
+        className={`w-full h-full fixed top-0 left-0 z-0 ${isOpen ? "" : "hidden"}`}
+        onClick={handleClose}
+      ></div>
+      <div>{"검색결과 : " + list?.length + "개"}</div>
+      {isOpen ? (
+        <div
+          key="list"
+          className={`absolute h-20 w-full z-10 overflow-y-auto bg-white`}
+        >
+          {isList ? (
+            <div>검색 결과가 없습니다.</div>
+          ) : (
+            <>
+              <ListItem list={renderItems} onClick={handleClick} />
+              {isNextList && <div ref={loader}></div>}
+            </>
+          )}
+        </div>
+      ) : (
+        <div onClick={handleOpen}> + </div>
+      )}
+    </>
+>>>>>>> Stashed changes
   );
 };
 
 export default ResultList;
+<<<<<<< Updated upstream
+=======
+
+const ListItem = ({
+  list,
+  onClick,
+}: {
+  list: SearchListItem[];
+  onClick: (value: string) => void;
+}) => (
+  <>
+    {list.map((item) => (
+      <div key={item.value}>
+        <div className="h-4" onClick={() => onClick(item.value)}>
+          {item.name}
+        </div>
+      </div>
+    ))}
+  </>
+);
+>>>>>>> Stashed changes
--- components/common/Finder/index.tsx
+++ components/common/Finder/index.tsx
@@ -7,39 +7,134 @@ export interface SearchListItem {
   value: string;
 }
 
-interface Props {}
-
 const TEST_LIST = [
-  { name: "1", value: "1" },
-  { name: "2", value: "2" },
-  { name: "3", value: "3" },
-  { name: "4", value: "4" },
-  { name: "5", value: "5" },
-  { name: "6", value: "6" },
-  { name: "7", value: "7" },
-  { name: "8", value: "8" },
-  { name: "9", value: "9" },
-  { name: "10", value: "10" },
-  { name: "11", value: "11" },
-  { name: "12", value: "12" },
-  { name: "13", value: "13" },
+  { name: "alpha", value: "alpha" },
+  { name: "bravo", value: "bravo" },
+  { name: "charlie", value: "charlie" },
+  { name: "delta", value: "delta" },
+  { name: "echo", value: "echo" },
+  { name: "foxtrot", value: "foxtrot" },
+  { name: "golf", value: "golf" },
+  { name: "hotel", value: "hotel" },
+  { name: "india", value: "india" },
+  { name: "juliet", value: "juliet" },
+  { name: "kilo", value: "kilo" },
+  { name: "lima", value: "lima" },
+  { name: "mike", value: "mike" },
+  { name: "november", value: "november" },
+  { name: "oscar", value: "oscar" },
+  { name: "papa", value: "papa" },
+  { name: "quebec", value: "quebec" },
+  { name: "romeo", value: "romeo" },
+  { name: "sierra", value: "sierra" },
+  { name: "tango", value: "tango" },
+  { name: "uniform", value: "uniform" },
+  { name: "victor", value: "victor" },
+  { name: "whiskey", value: "whiskey" },
+  { name: "x-ray", value: "x-ray" },
+  { name: "yankee", value: "yankee" },
+  { name: "zulu", value: "zulu" },
+  { name: "alpha1", value: "alpha1" },
+  { name: "bravo1", value: "bravo1" },
+  { name: "charlie1", value: "charlie1" },
+  { name: "delta1", value: "delta1" },
+  { name: "echo1", value: "echo1" },
+  { name: "foxtrot1", value: "foxtrot1" },
+  { name: "golf1", value: "golf1" },
+  { name: "hotel1", value: "hotel1" },
+  { name: "india1", value: "india1" },
+  { name: "juliet1", value: "juliet1" },
+  { name: "kilo1", value: "kilo1" },
+  { name: "lima1", value: "lima1" },
+  { name: "mike1", value: "mike1" },
+  { name: "november1", value: "november1" },
+  { name: "oscar1", value: "oscar1" },
+  { name: "papa1", value: "papa1" },
+  { name: "quebec1", value: "quebec1" },
+  { name: "romeo1", value: "romeo1" },
+  { name: "sierra1", value: "sierra1" },
+  { name: "tango1", value: "tango1" },
+  { name: "uniform1", value: "uniform1" },
+  { name: "victor1", value: "victor1" },
+  { name: "whiskey1", value: "whiskey1" },
+  { name: "x-ray1", value: "x-ray1" },
+  { name: "yankee1", value: "yankee1" },
+  { name: "zulu1", value: "zulu1" },
+  { name: "alpha2", value: "alpha2" },
+  { name: "bravo2", value: "bravo2" },
+  { name: "charlie2", value: "charlie2" },
+  { name: "delta2", value: "delta2" },
+  { name: "echo2", value: "echo2" },
+  { name: "foxtrot2", value: "foxtrot2" },
+  { name: "golf2", value: "golf2" },
+  { name: "hotel2", value: "hotel2" },
+  { name: "india2", value: "india2" },
+  { name: "juliet2", value: "juliet2" },
+  { name: "kilo2", value: "kilo2" },
+  { name: "lima2", value: "lima2" },
+  { name: "mike2", value: "mike2" },
+  { name: "november2", value: "november2" },
+  { name: "oscar2", value: "oscar2" },
+  { name: "papa2", value: "papa2" },
+  { name: "quebec2", value: "quebec2" },
+  { name: "romeo2", value: "romeo2" },
+  { name: "sierra2", value: "sierra2" },
+  { name: "tango2", value: "tango2" },
+  { name: "uniform2", value: "uniform2" },
+  { name: "victor2", value: "victor2" },
+  { name: "whiskey2", value: "whiskey2" },
+  { name: "x-ray2", value: "x-ray2" },
+  { name: "yankee2", value: "yankee2" },
+  { name: "zulu2", value: "zulu2" },
 ];
 
 function Finder() {
   const [search, setSearch] = useState("");
   const [searchResultList, setSearchResultList] = useState<SearchListItem[]>();
 
-  const debouncedValue = useDebounce(search, 300);
+  const [listCount, setListCount] = useState<number>(0);
+  const [sliceArray, setSliceArray] = useState<SearchListItem[]>();
+  const debouncedValue = useDebounce(search, 1000);
 
   const handleSearchInput = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setSearch(value);
   };
 
+  const handleClickListItem = (value: string) => {
+    setSearch(value);
+  };
+
   //추후 목업데이터 api 형식으로 추가
-  const getSearchResult = (value: string) => {
-    value && setSearchResultList(TEST_LIST);
-    return;
+  const getSearchResult = async (value: string) => {
+    const apiResearch = async (): Promise<SearchListItem[]> => {
+      let research: SearchListItem[] = [];
+      try {
+        TEST_LIST.forEach((item) => {
+          if (item.value.includes(value)) {
+            research.push(item);
+          }
+        });
+        return research;
+      } catch (e) {
+        console.log(e);
+      } finally {
+        return research;
+      }
+    };
+    const getResearch = async (): Promise<SearchListItem[]> => {
+      const result = await apiResearch();
+      return result;
+    };
+    const data: SearchListItem[] = await getResearch();
+    if (data) {
+      setSearchResultList(data);
+      setSliceArray(data);
+    }
+  };
+
+  const onChangeNextList = () => {
+    setListCount((prev) => prev + 1);
   };
 
   useEffect(() => {
@@ -49,9 +144,16 @@ function Finder() {
   return (
     <div className="relative">
       <div>
-        <input onChange={(e) => handleSearchInput(e)} />
+        <input value={search} onChange={(e) => handleSearchInput(e)} />
       </div>
-      {searchResultList && <ResultList list={searchResultList} />}
+      {searchResultList && (
+        <ResultList
+          list={sliceArray ?? []}
+          page={listCount}
+          onChangeNextList={onChangeNextList}
+          onClick={handleClickListItem}
+        />
+      )}
     </div>
   );
 }
