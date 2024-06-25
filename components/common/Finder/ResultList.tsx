20bdbb6c80946ddd48b5f8755068496c5aa20b50
jungwanhong <jungwan_hong@lodics.com>
2024-06-25 15:30:53 +0900

    fix - conflict 처리 하다 남은부분 수정

--- components/common/Finder/ResultList.tsx
+++ components/common/Finder/ResultList.tsx
@@ -1,31 +1,6 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 import { SearchListItem } from ".";
 
-<<<<<<< Updated upstream
-const ResultList = ({ list }: { list: SearchListItem[] }) => {
-  let viewList: SearchListItem[] = [];
-
-  const [count, setCount] = useState<number>(0);
-  const [sliceArray, setSliceArray] = useState<
-    { [key: number]: SearchListItem[] }[]
-  >([]);
-
-  const loader = useRef(null);
-
-  const arraySlicer = ({ list, count }: { list: any[]; count: number }) => {
-    let sliceArray = [];
-    for (let i = 0; i <= list.length; i++) {
-      sliceArray.push(list.splice(0, count));
-    }
-    setSliceArray(sliceArray);
-  };
-  const handleObserver = useCallback((entries: IntersectionObserverEntry[]) => {
-    const target = entries[0];
-    if (target.isIntersecting) {
-      setCount((prev) => prev + 1);
-    }
-  }, []);
-=======
 interface Props {
   list: SearchListItem[];
   page: number;
@@ -67,33 +42,10 @@ const ResultList = ({ list, page, onChangeNextList, onClick }: Props) => {
     },
     [onClick]
   );
->>>>>>> Stashed changes
 
   useEffect(() => {
     const option = {
       root: null,
-<<<<<<< Updated upstream
-      rootMargin: "20px",
-      threshold: 0,
-    };
-    const observer = new IntersectionObserver(handleObserver, option);
-    if (loader.current) observer.observe(loader.current);
-  });
-
-  arraySlicer({ list, count: 5 });
-
-  return (
-    <div className="h-20 overflow-y-auto absolute">
-      {viewList.map((item) => (
-        <>
-          <div key={item.value}>
-            <div className="h-4">{item.name}</div>
-          </div>
-          <div ref={loader}></div>
-        </>
-      ))}
-    </div>
-=======
       rootMargin: "0px",
       threshold: 0,
     };
@@ -139,13 +91,10 @@ const ResultList = ({ list, page, onChangeNextList, onClick }: Props) => {
         <div onClick={handleOpen}> + </div>
       )}
     </>
->>>>>>> Stashed changes
   );
 };
 
 export default ResultList;
-<<<<<<< Updated upstream
-=======
 
 const ListItem = ({
   list,
@@ -164,4 +113,3 @@ const ListItem = ({
     ))}
   </>
 );
->>>>>>> Stashed changes
